187bbd9f9da19c42a7feabb1e69a61e96080b8b1
S-K-E <su88cya@qq.com>
2024-10-24 21:15:45 +0800

    Site updated: 2024-10-24 21:15:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/24/hello-world/","2024/10/24/Markdown学习/"];function toRandomPost(){
+var posts=["2024/10/24/hello-world/","2024/10/24/Markdown学习/","2024/10/24/基本Dos命令/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
