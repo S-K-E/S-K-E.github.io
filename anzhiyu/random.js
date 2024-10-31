5dadb183f20786529d5e76c46280688a3901e0cc
S-K-E <su88cya@qq.com>
2024-10-31 20:37:56 +0800

    Site updated: 2024-10-31 20:37:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/25/Java入门与安装/","2024/10/25/Hello-world-java/","2024/10/24/Markdown学习/","2024/10/24/基本Dos命令/","2024/10/25/Java程序运行机制/"];function toRandomPost(){
+var posts=["2024/10/25/Hello-world-java/","2024/10/25/Java入门与安装/","2024/10/25/Java程序运行机制/","2024/10/24/基本Dos命令/","2024/10/24/Markdown学习/","2024/10/31/Java基础01/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
